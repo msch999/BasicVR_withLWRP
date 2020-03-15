58ddf8ebe8d7529d1f998c58ff7ec34d41f0d61e
msch <post_msch-git@yahoo.de>
2020-03-15 01:27:35 +0100

    Last commit before migating to 2020.1a. Added non-functional lying Script

---
+++ BasicVR_withLWRP/Assets/Scripts/Flying.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    public Transform head;
+
+    //public TrackedReference leftHand;
+    public TrackedReference rightHand;
+    public OVRInput.Controller
+
+    private bool isFlying = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 leftDir = leftHand.transform.position - head.position;
+        Vector3 rightDir = rightHand.transform.position - head.position;
+
+        Vector3 dir = leftDir + rightDir;
+
+        transform.position += dir;
+    }
+}
